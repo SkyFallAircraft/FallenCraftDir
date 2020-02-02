c2daf1f021cc82de3634286b9bdfd7296901f1f8
Vincent Allen <vallen813@gmail.com>
2020-02-01 21:45:31 -0800

    pause and crap

--- Assets/Scripts/Pause.cs
+++ Assets/Scripts/Pause.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 using System.Windows.Input;
+using UnityEngine.SceneManagement;
 
 public class Pause : MonoBehaviour
 {
@@ -10,10 +11,12 @@ public class Pause : MonoBehaviour
     public GameObject pauseFilter;
     public GameObject[] pauseButtons;
     GameManager gMan;
+    bool check = false;
 
     private void Start()
     {
         gameController = GetComponent<GameController>();
+        pauseFilter = GameObject.FindWithTag("PauseFilter");
         paused = false;
         PauseGame(paused);
     }
@@ -26,6 +29,16 @@ public class Pause : MonoBehaviour
             paused = !paused;
             PauseGame(paused);
         }
+        if(SceneManager.sceneCount < 2)
+        {
+            pauseFilter = GameObject.FindWithTag("pauseFilter");
+            if (pauseFilter) pauseFilter.SetActive(false);
+        }
+        if (gMan.loaded == false && check == false)
+        {
+            pauseFilter.SetActive(false);
+            check = false;
+        }
     }
 
     public void PauseGame(bool p)
@@ -33,7 +46,15 @@ public class Pause : MonoBehaviour
         if (p)
         {
             Time.timeScale = 0f;
-            pauseFilter.SetActive(true);
+            if (pauseFilter != null)
+            {
+                pauseFilter.SetActive(true);
+            }
+            else
+            {
+                pauseFilter = GameObject.FindWithTag("PauseFilter");
+                pauseFilter.SetActive(true);
+            }
             paused = true;
             gameController.OnPaused(paused);
             for (int i = 0; i < pauseButtons.Length; i++) pauseButtons[i].SetActive(true);
