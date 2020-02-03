63f36d70b91ebe7563b9d514a1c1f0583ecb1d90
lazerblimp <57571370+lazerblimp@users.noreply.github.com>
2020-02-02 16:50:55 -0800

    Jacob's stupid changes

---
+++ Assets/hookshot.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class hookshot : MonoBehaviour
+{
+    private Rigidbody2D rb;
+    public Transform topSpawnPosition;
+    public Transform rightSpawnPosition;
+    public Transform leftSpawnPosition;
+    public bool isUp = false;
+    public bool isRight = false;
+    public bool isLeft = false;
+    public float hookSpeed;
+    private bool isMovingUp = false;
+    
+    
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
